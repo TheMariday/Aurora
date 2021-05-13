28a6a63be3e1185357924472291287f2c0ac9170
Samuel Martin <martinsa0182@gmail.com>
2021-05-13 08:35:07 +0100

    working voice recognition!

---
+++ include/tef/aurora/speechRecognition.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "pocketsphinx/pocketsphinx.h"
+
+namespace TEF::Aurora {
+
+}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,134 @@
 #include <stdio.h>
-#include <chrono>
-#include <vector>
-#include <spdlog/spdlog.h>
-#include <atomic>
+#include <string.h>
+#include <assert.h>
+#include <thread>
 
-//#include "tef/aurora/effects/simpleEffect.h"
-//#include "tef/aurora/sound.h"
-//#include "tef/aurora/masterController.h"
-#include "tef/aurora/userControl.h"
+#include <sys/select.h>
 
-#define pause(x) std::this_thread::sleep_for(std::chrono::seconds(x));
+#include <sphinxbase/err.h>
+#include <sphinxbase/ad.h>
 
+#include "pocketsphinx.h"
+
+/*
+arecord -f cd -c 1 -r 16000 system_damage_report.wav
+aplay system_damage_report.wav -D "sysdefault:CARD=Audio"
+pocketsphinx_continuous -infile system_damage_report.wav  -jsgf ~/temp/pocketsphinx/test.gram
+*/
+
+static const arg_t cont_args_def[] = {
+    POCKETSPHINX_OPTIONS,
+    /* Argument file. */
+    {"-argfile",
+     ARG_STRING,
+     NULL,
+     "Argument file giving extra arguments."},
+    {"-adcdev",
+     ARG_STRING,
+     NULL,
+     "Name of audio device to use for input."},
+    {"-infile",
+     ARG_STRING,
+     NULL,
+     "Audio file to transcribe."},
+    {"-inmic",
+     ARG_BOOLEAN,
+     "no",
+     "Transcribe audio from microphone."},
+    {"-time",
+     ARG_BOOLEAN,
+     "no",
+     "Print word times in file transcription."},
+    CMDLN_EMPTY_OPTION
+};
+
+static ps_decoder_t* ps;
+static cmd_ln_t* config;
+static FILE* rawfd;
+
+
+/*
+ * Main utterance processing loop:
+ *     for (;;) {
+ *        start utterance and wait for speech to process
+ *        decoding till end-of-utterance silence will be detected
+ *        print utterance result;
+ *     }
+ */
+static void recognize_from_microphone()
+{
+    ad_rec_t* ad;
+    int16 adbuf[2048];
+    uint8 utt_started, in_speech;
+    int32 k;
+    char const* hyp;
+
+    if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),
+        (int)cmd_ln_float32_r(config,
+            "-samprate"))) == NULL)
+        E_FATAL("Failed to open audio device\n");
+    if (ad_start_rec(ad) < 0)
+        E_FATAL("Failed to start recording\n");
+
+    if (ps_start_utt(ps) < 0)
+        E_FATAL("Failed to start utterance\n");
+    utt_started = FALSE;
+    E_INFO("Ready....\n");
+
+    for (;;) {
+        if ((k = ad_read(ad, adbuf, 2048)) < 0)
+            E_FATAL("Failed to read audio\n");
+        ps_process_raw(ps, adbuf, k, FALSE, FALSE);
+        in_speech = ps_get_in_speech(ps);
+        if (in_speech && !utt_started) {
+            utt_started = TRUE;
+            E_INFO("Listening...\n");
+        }
+        if (!in_speech && utt_started) {
+            /* speech -> silence transition, time to start new utterance  */
+            ps_end_utt(ps);
+            hyp = ps_get_hyp(ps, NULL);
+            if (hyp != NULL) {
+                printf("%s\n", hyp);
+                fflush(stdout);
+            }
+
+            if (ps_start_utt(ps) < 0)
+                E_FATAL("Failed to start utterance\n");
+            utt_started = FALSE;
+            E_INFO("Ready....\n");
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds(100));
+    }
+    ad_close(ad);
+}
+
+int main(int argc, char* argv[])
+{
+    char const* cfg;
+
+    config = cmd_ln_init(NULL, cont_args_def, TRUE, "-inmic", "yes", "-jsgf", "/home/pi/temp/pocketsphinx/test.gram", NULL);
+
+    ps_default_search_args(config);
+    ps = ps_init(config);
+    if (ps == NULL) {
+        cmd_ln_free_r(config);
+        return 1;
+    }
+
+    E_INFO("%s COMPILED ON: %s, AT: %s\n\n", argv[0], __DATE__, __TIME__);
+
+    recognize_from_microphone();
+
+
+    ps_free(ps);
+    cmd_ln_free_r(config);
+
+    return 0;
+}
+/*
+*
+*
 struct SystemDummy
 {
 	bool Reboot() {
@@ -35,13 +153,9 @@ struct SystemDummy
 	}
 };
 
-
-
-
-int main(int argc, char** argv)
-{
-	spdlog::set_level(spdlog::level::debug);
-
+*
+*
+*
 	SystemDummy sys;
 
 	TEF::Aurora::UserControl uc;
@@ -62,17 +176,6 @@ int main(int argc, char** argv)
 	while (running) {
 		sleep(1);
 	}
-	spdlog::debug("done");
-
-}
-
-
-
-/*
-*
-*
-*
-*
 * 	uc.RegisterVoid("system reboot", [&sys]() {return sys.Reboot(); });
 	//uc.RegisterVoid("system reboot 2", [&sys]() {return sys.Reboot(); });
 	uc.RegisterBool("system safety", [&sys](bool b) {return sys.SetSafety(b); });
